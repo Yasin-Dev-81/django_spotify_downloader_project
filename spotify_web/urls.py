d4f7a1c6c2aa9370cae97ebd11cb0500f4aa1626
Yasin Den <76453371+Yasin-Den@users.noreply.github.com>
2022-07-04 17:38:10 +0430

    Update urls.py

--- spotify_web/urls.py
+++ spotify_web/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search, name='spotify_search_url'),
-    path('download/<str:type>/<str:id>', views.download, name='spotify_download_url'),
+    path('search/<str:selected_search_type>', views.search_view, name='spotify_search_url'),
+    path('detail/<str:search_type>/<str:search_id>', views.detail_view, name='spotify_detail_url'),
+    path('download/<str:song_id>/', views.song_download_view, name='spotify_download_url'),
 ]
