972c557c0b129e57f4ada57a9d8474ee560bfde5
Yasin Den <76453371+Yasin-Den@users.noreply.github.com>
2022-07-04 17:42:02 +0430

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('spotify/', include('spotify_web.urls')),
+    path('', include('home.urls')),
 ]
